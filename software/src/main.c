60d9b6316385ad2979688d7c965365e59e06ed29
Peter McGoron <code@mcgoron.com>
2022-07-13 14:12:06 -0400

    testing PMOD DAC output successful

--- software/src/main.c
+++ software/src/main.c
@@ -11,10 +11,16 @@ main(void)
 	LOG_PRINTK("hello, world\n");
 	for (;;) {
 		k_sleep(K_MSEC(1000));
+#if 0 // ADC code
 		*adc_conv[0] = v;
 		v = !v;
 		r = *adc_sdo[0];
-
+#endif 
+		*dac_ctrl[0] = v;
+		v++;
+		if (v == 8)
+			v = 0;
+		r = *dac_miso[0];
 		LOG_PRINTK("out: %d; in: %d\n", v, r);
 	}
 }
