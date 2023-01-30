556db1f361e2aadd369eb8d811fab2db8a001e6e
Shell-ac <113789606+Shell-ac@users.noreply.github.com>
2023-01-30 14:09:49 -0500

    Add files via upload
    Verilog signal propagation testbench for the intsat module

---
+++ firmware/rtl/control_loop/intsat_testbench.v
@@ -0,0 +1,78 @@
+//testbench for intsat module
+//Timothy Burman, 2022
+
+module intsat_testbench
+#(
+
+	parameter IN_LEN = 64,
+	parameter LTRUNC = 32
+
+	
+)
+(
+	//Outputs
+	output signed [IN_LEN-LTRUNC-1:0] outp
+);
+
+
+
+reg signed [IN_LEN-1:0] inp;
+
+intsat testbench (inp, outp);
+
+initial
+begin
+
+	//intial values
+	inp = 64'd410000000;
+
+	#10;
+
+	inp = inp + 1;
+	#10;
+
+	inp = inp + 1;
+	#10;
+
+	inp = inp + 1;
+	#10;
+
+	inp = inp + 1;
+	#10;
+
+	inp = inp + 1;
+	#10;
+
+	inp = inp + 1;
+	#10;
+
+	inp = inp + 10000000;
+	#10;
+
+	inp = inp + 10000000;
+	#10;
+
+
+	inp = inp - 1000000;
+	#10;
+
+	inp = -64'd1000000000;
+
+	#10;
+
+	inp = inp - 400095;
+
+	#10;
+
+
+
+
+	$finish;
+
+
+end
+
+
+
+
+endmodule
