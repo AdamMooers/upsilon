f6a4a6bbea15933e5a2b96af453bf491989c7016
Peter McGoron <code@mcgoron.com>
2023-05-11 12:08:12 -0400

    add setup_client.sh

---
+++ doc/setup_client.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $(id -u) != 0 ]; then
+	echo 'script must be run as root'
+	exit 1
+fi
+
+INTER="$1"
+BINARY="$(realpath $2)"
+
+ip link set "$INTER" up
+ip addr add 192.168.1.100/24 dev "$INTER"
+BOOTDIR=$(mktemp -d)
+cp "$BINARY" "$BOOTDIR"
+cd "$BOOTDIR"
+
+dnsmasq -d --port=0 --enable-tftp --tftp-root="$BOOTDIR" --user=root --group=root --interface="$INTER"
