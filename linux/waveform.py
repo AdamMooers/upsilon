df9a7dcd8db2f1d2dc6b7988a340d3057ad5ed3d
Peter McGoron <code@mcgoron.com>
2024-03-12 01:13:23 +0000

    Precompile Micropython to bytecode: Document
    This reduces load times a little bit, and also allows for documentation
    inside the modules without bloating up memory.

--- linux/waveform.py
+++ linux/waveform.py
@@ -9,20 +9,35 @@ class Waveform(Immutable):
 
         self.make_immutable()
 
-    def run_waveform(self, wf, timer, timer_spacing, do_loop):
+    def run(self, wf, timer_spacing, do_loop = False):
+        """ Start waveform with signal.
+
+        :param wf: Array of integers that describe the waveform.
+           These are twos-complement 20-bit integers.
+        :param timer_spacing: The amount of time to wait between
+           points on the waveform.
+        :param do_loop: If True, the waveform will repeat.
+        """
+        self.stop()
+
+        self.ram_pi.v = 0
+        self.ram.mem32.load(wf)
+
+        self.regs.wform_width.v = len(wf)
+        self.regs.timer_spacing.v = timer_spacing
+
+        self.regs.do_loop.v = do_loop
+        self.ram_pi.v = 1
+        self.regs.run.v = 1
+
+    def stop(self):
+        """ Stop the waveform and wait until it is ready. """
         self.regs.run = 0
         self.regs.do_loop = 0
 
         while self.regs.finished_or_ready == 0:
             pass
 
-        self.ram_pi.v = 0
-        self.ram.load(wf)
-
-        self.regs.wform_width.v = len(wf)
-        self.regs.timer.v = timer
-        self.regs.timer_spacing.v = timer_spacing
-
-        self.regs.do_loop.v = do_loop
-        self.ram_pi.v = 1
-        self.regs.run.v = 1
+    def dump(self):
+        """ Dump contents of control registers. """
+        return self.regs.dump()
