a57db7ab2ec1941b9ed1b5eb9e1f00d11a22f23e
Adam Mooers <21245397+AdamMooers@users.noreply.github.com>
2024-06-22 22:10:03 -0400

    Added in basic ADC reads

--- swic/test.c
+++ swic/test.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include "pico0_mmio.h"
 
-#define DELAY (uint32_t)(1000)
+#define DELAY (uint32_t)(10000)
 #define DAC_WRITE_MASK (uint32_t)(0x100000)
 #define OUTPUT (uint32_t *)(0x10100)
 #define I (uint32_t *)(0x10104)
@@ -9,20 +9,31 @@
 void _start(void)
 {
 	*OUTPUT = 0;
+	*PARAMS_ZPOS = 0;
 
 	for (;;)
 	{
 		*OUTPUT = (*OUTPUT + 128)%(1 << 16);
 
-		*PARAMS_ZPOS = *OUTPUT;
-
 		// Wait for the DAC to become available
-		//while (!*DAC0_WAIT_FINISHED_OR_READY);
+		//while (*DAC0_WAIT_FINISHED_OR_READY == 0);
 
+		// Write voltage to the DAC
 		*DAC0_TO_SLAVE = DAC_WRITE_MASK | *OUTPUT;
 		*DAC0_ARM = 1;
 		*DAC0_ARM = 0;
 
+		// Request voltage from the ADC
+		*ADC0_TO_SLAVE = 0;
+		*ADC0_ARM = 1;
+		*ADC0_ARM = 0;
+
+		//while (*ADC0_WAIT_FINISHED_OR_READY == 0) {
+		//	*PARAMS_ZPOS += 1;
+		//}
+
 		for (*I=0;*I<DELAY;(*I)++);
+
+		*PARAMS_ZPOS = *ADC0_FROM_SLAVE;
 	}
 }
