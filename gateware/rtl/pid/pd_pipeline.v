04f468fcddcbe4c866242d8639a25b4faa943516
Adam Mooers <21245397+AdamMooers@users.noreply.github.com>
2024-07-10 15:59:31 -0400

    Added verilator makefile and single shot pipelining

--- gateware/rtl/pid/pd_pipeline.v
+++ gateware/rtl/pid/pd_pipeline.v
@@ -3,47 +3,85 @@
  * This file is a part of Upsilon, a free and open source software project.
  * For license terms, refer to the files in `doc/copying` in the Upsilon
  * source distribution.
+ *
  */
 
-module waveform #() (
+module pd_pipeline #(
+	parameter INPUT_WIDTH = 18,
+	parameter OUTPUT_WIDTH = 32
+) (
 	input clk,
-	input signed [1:0] reset,
+	input reset,
+	input start,
 
-	input signed [18-1:0] kp,
-	input signed [18-1:0] ki,
-	input signed [9-1:0] dt,
-	input signed [18-1:0] setpoint,
-	input signed [18-1:0] actual,
+	input signed [INPUT_WIDTH-1:0] i_kp,
+	input signed [INPUT_WIDTH-1:0] i_ki,
+	input signed [INPUT_WIDTH-1:0] i_setpoint,
+	input signed [INPUT_WIDTH-1:0] i_actual,
 
-	output signed reg [36-1:0] integral,
-	output signed [36-1:0] result
+	output reg signed [OUTPUT_WIDTH-1:0] o_integral,
+	output reg signed [OUTPUT_WIDTH-1:0] o_pd_out,
+	output complete
 );
 
-	reg [18-1:0] error;
-	reg [36-1:0] weighted_integral;
-	reg [36-1:0] weighted_proportional;
-	reg [36-1:0] adjustment;
+	reg [INPUT_WIDTH-1:0] error;
+	reg [OUTPUT_WIDTH-1:0] weighted_integral;
+	reg [OUTPUT_WIDTH-1:0] weighted_proportional;
 
-// TODO: Get reset working
-// TODO: Parameterize the inputs and outputs
-
-	always @(posedge clk) begin
-		error <= actual - setpoint;
-	end
+	reg start_delay;
+	reg ce_stage_1;
+	reg ce_stage_2;
+	reg ce_stage_3;
+	reg ce_stage_4;
 
 	always @(posedge clk) begin
-		integral <= integral + error;
+		start_delay <= start;
 	end
 
+	// Stage 0
 	always @(posedge clk) begin
-		weighted_integral <= integral * ki;
-		weighted_proportional <= error * kp;
+		// The pipeline runs only once on the positive edge of start
+		if (start & ~start_delay) begin
+			error <= i_actual - i_setpoint;
+			ce_stage_1 <= start;
+		end
 	end
 
+	// Stage 1
 	always @(posedge clk) begin
-		adjustment <= weighted_integral + weighted_proportional
+		if (reset) begin
+			o_integral <= 0;
+		end else if (ce_stage_1) begin
+			o_integral <= o_integral + {{OUTPUT_WIDTH-INPUT_WIDTH{error[INPUT_WIDTH-1]}},error};
+			ce_stage_2 <= ce_stage_1;
+		end
 	end
 
-	assign result = adjustment
+	// Stage 2
+	always @(posedge clk) begin
+		if (ce_stage_2) begin
+			weighted_integral <= o_integral * i_ki;
+			weighted_proportional <= error * i_kp;
+			ce_stage_3 <= ce_stage_2;
+		end
+	end
+
+	// Stage 3
+	always @(posedge clk) begin
+		if (ce_stage_3) begin
+			o_pd_out <= weighted_integral + weighted_proportional;
+			ce_stage_4 <= ce_stage_3;
+		end
+	end
+
+	assign complete = ce_stage_4;
+
+// Request ADC output
+
+// Wait for ADC output
+// Update actual
+// Wait for pipeline to propagate result
+// Request ADC output (maybe add update&arm?)
+// Set DAC to result (maybe add update&arm?)
 
 endmodule
