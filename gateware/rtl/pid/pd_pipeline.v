a3d0e49f8ad674b1b6f58274745aa3372d6efbcf
Adam Mooers <21245397+AdamMooers@users.noreply.github.com>
2024-07-10 22:27:33 -0400

    Made pipeline stateless to remove offload FSM work to processor

--- gateware/rtl/pid/pd_pipeline.v
+++ gateware/rtl/pid/pd_pipeline.v
@@ -10,76 +10,51 @@ module pd_pipeline #(
 	parameter INPUT_WIDTH = 18,
 	parameter OUTPUT_WIDTH = 32
 ) (
-	input clk,
-	input reset,
-	input start,
+	input i_clk,
 
 	input signed [INPUT_WIDTH-1:0] i_kp,
 	input signed [INPUT_WIDTH-1:0] i_ki,
 	input signed [INPUT_WIDTH-1:0] i_setpoint,
 	input signed [INPUT_WIDTH-1:0] i_actual,
+	input signed [OUTPUT_WIDTH-1:0] i_integral,
 
-	output reg signed [OUTPUT_WIDTH-1:0] o_integral,
-	output reg signed [OUTPUT_WIDTH-1:0] o_pd_out,
-	output complete
+	output signed [OUTPUT_WIDTH-1:0] o_integral,
+	output reg signed [OUTPUT_WIDTH-1:0] o_pd_out
 );
 
 	reg [INPUT_WIDTH-1:0] error;
+	reg [OUTPUT_WIDTH-1:0] updated_integral;
 	reg [OUTPUT_WIDTH-1:0] weighted_integral;
 	reg [OUTPUT_WIDTH-1:0] weighted_proportional;
 
-	reg start_delay;
-	reg ce_stage_1;
-	reg ce_stage_2;
-	reg ce_stage_3;
-	reg ce_stage_4;
-
-	always @(posedge clk) begin
-		start_delay <= start;
-	end
-
 	// Stage 0
-	always @(posedge clk) begin
-		// The pipeline runs only once on the positive edge of start
-		if (start & ~start_delay) begin
-			error <= i_actual - i_setpoint;
-			ce_stage_1 <= start;
-		end
+	always @(posedge i_clk) begin
+		error <= i_actual - i_setpoint;
 	end
 
 	// Stage 1
-	always @(posedge clk) begin
-		if (reset) begin
-			o_integral <= 0;
-		end else if (ce_stage_1) begin
-			o_integral <= o_integral + {{OUTPUT_WIDTH-INPUT_WIDTH{error[INPUT_WIDTH-1]}},error};
-			ce_stage_2 <= ce_stage_1;
-		end
+	always @(posedge i_clk) begin
+		updated_integral <= i_integral + {{OUTPUT_WIDTH-INPUT_WIDTH{error[INPUT_WIDTH-1]}},error};
 	end
 
 	// Stage 2
-	always @(posedge clk) begin
-		if (ce_stage_2) begin
-			weighted_integral <= o_integral * i_ki;
-			weighted_proportional <= error * i_kp;
-			ce_stage_3 <= ce_stage_2;
-		end
+	always @(posedge i_clk) begin
+		weighted_integral <= updated_integral * i_ki;
+		weighted_proportional <= error * i_kp;
 	end
 
 	// Stage 3
-	always @(posedge clk) begin
-		if (ce_stage_3) begin
-			o_pd_out <= weighted_integral + weighted_proportional;
-			ce_stage_4 <= ce_stage_3;
-		end
+	always @(posedge i_clk) begin
+		o_pd_out <= weighted_integral + weighted_proportional;
 	end
 
-	assign complete = ce_stage_4;
+	assign o_integral = updated_integral;
 
 // Request ADC output
 
 // Wait for ADC output
 // Update actual
+// Update integral
 // Wait for pipeline to propagate result
 // Request ADC output (maybe add update&arm?)
 // Set DAC to result (maybe add update&arm?)
