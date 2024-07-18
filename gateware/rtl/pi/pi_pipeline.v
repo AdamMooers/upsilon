b4fff4f1f543c85dad4d23aef9131d306ee79663
AdamMooers <21245397+AdamMooers@users.noreply.github.com>
2024-07-18 20:49:15 +0000

    Increased the output width to work with any possible input

--- gateware/rtl/pi/pi_pipeline.v
+++ gateware/rtl/pi/pi_pipeline.v
@@ -10,43 +10,48 @@ module pi_pipeline #(
 	parameter INPUT_WIDTH = 18,
 	parameter OUTPUT_WIDTH = 32
 ) (
-	input clk, // clk instead of i_clk for verilog testbench compatibility
+	input clk, 
 
-	input signed [INPUT_WIDTH-1:0] kp,
-	input signed [INPUT_WIDTH-1:0] ki,
+	input signed [OUTPUT_WIDTH-1:0] kp,
+	input signed [OUTPUT_WIDTH-1:0] ki,
 	input signed [INPUT_WIDTH-1:0] setpoint,
 	input signed [INPUT_WIDTH-1:0] actual,
 	input signed [OUTPUT_WIDTH-1:0] integral_input,
+	input signed [OUTPUT_WIDTH-1:0] pi_result_lower_bound,
+	input signed [OUTPUT_WIDTH-1:0] pi_result_upper_bound,
 
 	output signed [OUTPUT_WIDTH-1:0] integral_result,
 	output reg signed [OUTPUT_WIDTH-1:0] pi_result
 );
 
+	localparam integer UNCLAMPED_PI_RESULT_WIDTH = OUTPUT_WIDTH*2;
+
 	reg [OUTPUT_WIDTH-1:0] error;
 	reg [OUTPUT_WIDTH-1:0] updated_integral;
-	reg [OUTPUT_WIDTH-1:0] weighted_integral;
-	reg [OUTPUT_WIDTH-1:0] weighted_proportional;
+	reg [UNCLAMPED_PI_RESULT_WIDTH-1:0] weighted_integral;
+	reg [UNCLAMPED_PI_RESULT_WIDTH-1:0] weighted_proportional;
+	reg [UNCLAMPED_PI_RESULT_WIDTH-1:0] pi_result_unclamped;
 
-	// Stage 0
+	// Stage 1
 	always @(posedge clk) begin
 		error <= {{OUTPUT_WIDTH-INPUT_WIDTH{actual[INPUT_WIDTH-1]}},actual} -
 		{{OUTPUT_WIDTH-INPUT_WIDTH{setpoint[INPUT_WIDTH-1]}},setpoint};
 	end
 
-	// Stage 1
+	// Stage 2
 	always @(posedge clk) begin
 		updated_integral <= integral_input + error;
 	end
 
-	// Stage 2
-	always @(posedge clk) begin
-		weighted_integral <= updated_integral * {{OUTPUT_WIDTH-INPUT_WIDTH{ki[INPUT_WIDTH-1]}},ki};
-		weighted_proportional <= error * {{OUTPUT_WIDTH-INPUT_WIDTH{kp[INPUT_WIDTH-1]}},kp};
-	end
-
 	// Stage 3
 	always @(posedge clk) begin
-		pi_result <= weighted_integral + weighted_proportional;
+		weighted_integral <= updated_integral * {{UNCLAMPED_PI_RESULT_WIDTH-OUTPUT_WIDTH{ki[OUTPUT_WIDTH-1]}},ki};
+		weighted_proportional <= error * {{UNCLAMPED_PI_RESULT_WIDTH-OUTPUT_WIDTH{kp[OUTPUT_WIDTH-1]}},kp};
+	end
+
+	// Stage 4
+	always @(posedge clk) begin
+		pi_result_unclamped <= weighted_integral + weighted_proportional;
 	end
 
 	assign integral_result = updated_integral;
