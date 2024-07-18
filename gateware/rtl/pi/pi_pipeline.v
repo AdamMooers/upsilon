89b43462722d7f8560ccc0928e17ab85e349bcaa
AdamMooers <21245397+AdamMooers@users.noreply.github.com>
2024-07-18 21:43:07 +0000

    Added 5th pipeline stage for saturation. Fixed signed register bug

--- gateware/rtl/pi/pi_pipeline.v
+++ gateware/rtl/pi/pi_pipeline.v
@@ -8,7 +8,8 @@
 
 module pi_pipeline #(
 	parameter INPUT_WIDTH = 18,
-	parameter OUTPUT_WIDTH = 32
+	parameter OUTPUT_WIDTH = 32,
+	parameter OUTPUT_SATURATION_BITS = 20
 ) (
 	input clk, 
 
@@ -17,20 +18,19 @@ module pi_pipeline #(
 	input signed [INPUT_WIDTH-1:0] setpoint,
 	input signed [INPUT_WIDTH-1:0] actual,
 	input signed [OUTPUT_WIDTH-1:0] integral_input,
-	input signed [OUTPUT_WIDTH-1:0] pi_result_lower_bound,
-	input signed [OUTPUT_WIDTH-1:0] pi_result_upper_bound,
 
 	output signed [OUTPUT_WIDTH-1:0] integral_result,
 	output reg signed [OUTPUT_WIDTH-1:0] pi_result
 );
-
 	localparam integer UNCLAMPED_PI_RESULT_WIDTH = OUTPUT_WIDTH*2;
+	localparam integer PI_SATURATION_LOWER_BOUND = -(1 << OUTPUT_SATURATION_BITS);
+	localparam integer PI_SATURATION_UPPER_BOUND = (1 << OUTPUT_SATURATION_BITS) - 1;
 
-	reg [OUTPUT_WIDTH-1:0] error;
-	reg [OUTPUT_WIDTH-1:0] updated_integral;
-	reg [UNCLAMPED_PI_RESULT_WIDTH-1:0] weighted_integral;
-	reg [UNCLAMPED_PI_RESULT_WIDTH-1:0] weighted_proportional;
-	reg [UNCLAMPED_PI_RESULT_WIDTH-1:0] pi_result_unclamped;
+	reg signed [OUTPUT_WIDTH-1:0] error;
+	reg signed [OUTPUT_WIDTH-1:0] updated_integral;
+	reg signed [UNCLAMPED_PI_RESULT_WIDTH-1:0] weighted_integral;
+	reg signed [UNCLAMPED_PI_RESULT_WIDTH-1:0] weighted_proportional;
+	reg signed [UNCLAMPED_PI_RESULT_WIDTH-1:0] pi_result_unclamped;
 
 	// Stage 1
 	always @(posedge clk) begin
@@ -53,6 +53,20 @@ module pi_pipeline #(
 	always @(posedge clk) begin
 		pi_result_unclamped <= weighted_integral + weighted_proportional;
 	end
+
+	// Stage 5
+	always @(posedge clk) begin
+		if (weighted_integral > PI_SATURATION_UPPER_BOUND) begin
+			pi_result <= PI_SATURATION_UPPER_BOUND;
+		end
+		else if (weighted_integral < PI_SATURATION_LOWER_BOUND) begin
+			pi_result <= PI_SATURATION_LOWER_BOUND;
+		end
+		else begin
+			pi_result <= pi_result_unclamped[OUTPUT_WIDTH-1:0];
+		end
+	end
+
 
 	assign integral_result = updated_integral;
 
