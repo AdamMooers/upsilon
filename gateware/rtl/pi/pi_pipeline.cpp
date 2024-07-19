20611c2c318e6ba0ebaef384c04d5b6f627c7226
AdamMooers <21245397+AdamMooers@users.noreply.github.com>
2024-07-19 18:56:01 +0000

    Added preliminary support for saturation

--- gateware/rtl/pi/pi_pipeline.cpp
+++ gateware/rtl/pi/pi_pipeline.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <algorithm>
 #include "Vpi_pipeline.h"
 #include "../testbench.hpp"
 
 
 class PIPipelineTestbench : public TB<Vpi_pipeline> {
 	public:
-		void start_test(int32_t, int32_t, int32_t, int32_t, int32_t);
+		void run_test(int32_t, int32_t, int32_t, int32_t, int32_t);
 		void dump_inputs();
 		void dump_outputs();
 		PIPipelineTestbench(int _bailout = 0) : TB<Vpi_pipeline>(_bailout) {}
@@ -20,18 +21,52 @@ class PIPipelineTestbench : public TB<Vpi_pipeline> {
 void PIPipelineTestbench::posedge() {
 }
 
-void PIPipelineTestbench::start_test(
+void PIPipelineTestbench::run_test(
 	int32_t kp, 
 	int32_t ki, 
 	int32_t setpoint, 
 	int32_t actual, 
 	int32_t integral_input) {
+
 	mod.kp = kp;
 	mod.ki = ki;
 	mod.setpoint = setpoint;
 	mod.actual = actual;
 	mod.integral_input = integral_input;
-	run_clock();
+
+	// Let the pipeline run through all 5 stages
+	for (int j = 0; j<5+1; j++) {
+		this->run_clock();
+	}
+
+	int32_t expected_error = actual - setpoint;
+	int32_t expected_integral_result = integral_input + expected_error;
+	int64_t expected_unsaturated_pi_result = 
+		kp*static_cast<int64_t>expected_error + 
+		ki*static_cast<int64_t>expected_integral_result;
+	
+	//TODO: Remove the magic numbers
+	int32_t expected_pi_result = static_cast<int32_t>std::clamp(
+		expected_unsaturated_pi_result,
+		static_cast<int64_t>(-1^20),
+		static_cast<int64_t>(1^20-1)
+	);
+
+	if (static_cast<int32_t>(mod.integral_result) != expected_integral_result) {
+		this->dump_inputs();
+		this->dump_outputs();
+		throw std::logic_error(
+			"Output integral calculation did yield the expected value. (expected mod.integral_result = "+
+			std::to_string(expected_integral_result)+")");
+	}
+
+	if (static_cast<int32_t>(mod.pi_result) != expected_pi_result) {
+		this->dump_inputs();
+		this->dump_outputs();
+		throw std::logic_error(
+			"PI calculation did not yield the expected value. (expected mod.pi_result = "+
+			std::to_string(expected_pi_result)+")");
+	}
 }
 
 void PIPipelineTestbench::dump_outputs() {
@@ -68,8 +103,11 @@ int main(int argc, char *argv[]) {
 
 	auto adc_dist = std::uniform_int_distribution<int32_t>(-(1 << 17),(1 << 17) - 1);
 
-	// We limit these to 14 bits to ensure we don't overflow pi_result
-	auto pi_dist = std::uniform_int_distribution<int32_t>(-(1 << 14),(1 << 14) - 1);
+	// This value will be clamped by the SWIC to the provided range
+	auto int_dist = std::uniform_int_distribution<int32_t>(-(1 << 19),(1 << 19) - 1);
+
+	// These are set to scale across the entire range of the DAC output
+	auto pi_dist = std::uniform_int_distribution<int32_t>(-(1 << 19),(1 << 19) - 1);
 
 	for (int i = 1; i < NUM_INCRS; i++) {
 		uint32_t kp = pi_dist(engine);
@@ -78,32 +116,7 @@ int main(int argc, char *argv[]) {
 		int32_t actual = adc_dist(engine);
 		int32_t integral_input = adc_dist(engine);
 
-		tb->start_test(kp, ki, setpoint, actual, integral_input);
-
-		// Let the pipeline run through all 4 stages
-		for (int j = 0; j<4; j++) {
-			tb->run_clock();
-		}
-
-		int32_t expected_error = actual - setpoint;
-		int32_t expected_integral_result = integral_input + expected_error;
-		int32_t expected_pi_result =  kp*expected_error + ki*expected_integral_result;
-
-		if (static_cast<int32_t>(tb->mod.integral_result) != expected_integral_result) {
-			tb->dump_inputs();
-			tb->dump_outputs();
-			throw std::logic_error(
-				"Output integral calculation did yield the expected value. (expected mod.integral_result = "+
-				std::to_string(expected_integral_result)+")");
-		}
-
-		if (static_cast<int32_t>(tb->mod.pi_result) != expected_pi_result) {
-			tb->dump_inputs();
-			tb->dump_outputs();
-			throw std::logic_error(
-				"PI calculation did not yield the expected value. (expected mod.pi_result = "+
-				std::to_string(expected_pi_result)+")");
-		}
+		tb->run_test(kp, ki, setpoint, actual, integral_input);
 	}
 
 	return 0;
