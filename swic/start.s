031a3428146f65a19078af15173c96197c4d55e8
AdamMooers <21245397+AdamMooers@users.noreply.github.com>
2024-07-31 22:41:01 +0000

    Added global start to assembly to make symbol available to linker

--- swic/start.s
+++ swic/start.s
@@ -1,4 +1,5 @@
-.section ._start
+.globl start
+.section .start
 
 # The ISR starts at 0x10010 so we need to keep the
 # start routine limited to 4 instructions or less.
@@ -6,7 +7,7 @@
 # from running the ISR if we return from the main function.
 
 start:
-call main
+    call main
 
 loop:
-j loop
+    j loop
