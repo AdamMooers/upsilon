4233cca00d148ef5890828ad1cab2be8b3ef4ab2
AdamMooers <21245397+AdamMooers@users.noreply.github.com>
2024-07-31 22:42:53 +0000

    Added IRQ handler

--- swic/main.c
+++ swic/main.c
@@ -3,23 +3,25 @@
 
 #define DELAY (uint32_t)(10000)
 #define DAC_WRITE_MASK (uint32_t)(0x100000)
-#define OUTPUT (uint32_t *)(0x10100)
+//#define OUTPUT (uint32_t *)(0x10100)
 #define I (uint32_t *)(0x10104)
 
-void _start(void)
+void IRQ_handler(uint32_t irq_mask) __attribute__((section(".IRQ_handler")));
+
+void main(void)
 {
-	*OUTPUT = 0;
+	//*OUTPUT = 0;
 	*PARAMS_ZPOS = 0;
 
 	for (;;)
 	{
-		*OUTPUT = (*OUTPUT + 128)%(1 << 16);
+		//*OUTPUT = (*OUTPUT + 128)%(1 << 16);
 
 		// Wait for the DAC to become available
 		//while (*DAC0_WAIT_FINISHED_OR_READY == 0);
 
 		// Write voltage to the DAC
-		*DAC0_TO_SLAVE = DAC_WRITE_MASK | *OUTPUT;
+		//*DAC0_TO_SLAVE = DAC_WRITE_MASK | *OUTPUT;
 		*DAC0_ARM = 1;
 		*DAC0_ARM = 0;
 
@@ -34,6 +36,12 @@ void _start(void)
 
 		for (*I=0;*I<DELAY;(*I)++);
 
-		*PARAMS_ZPOS = *ADC0_FROM_SLAVE;
+		//*PARAMS_ZPOS = *ADC0_FROM_SLAVE;
 	}
+}
+
+void IRQ_handler(uint32_t irq_mask)
+{
+	*DAC0_ARM = 1;
+	*DAC0_ARM = 0;
 }
