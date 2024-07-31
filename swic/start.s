5eebbc8b79a8d5aea1afa37735f6c9dc17c6dff4
AdamMooers <21245397+AdamMooers@users.noreply.github.com>
2024-07-31 18:39:55 +0000

    Added start point for the swic

---
+++ swic/start.s
@@ -0,0 +1,12 @@
+.section ._start
+
+# The ISR starts at 0x10010 so we need to keep the
+# start routine limited to 4 instructions or less.
+# The loop command is a safeguard to prevent the SWIC 
+# from running the ISR if we return from the main function.
+
+start:
+call main
+
+loop:
+j loop
