152993632628cb2f9f8cb1ca571ef8a78fb60e93
AdamMooers <21245397+AdamMooers@users.noreply.github.com>
2024-08-01 22:44:12 +0000

    Set up main.c for dac test

--- swic/main.c
+++ swic/main.c
@@ -3,25 +3,24 @@
 
 #define DELAY (uint32_t)(10000)
 #define DAC_WRITE_MASK (uint32_t)(0x100000)
-//#define OUTPUT (uint32_t *)(0x10100)
-#define I (uint32_t *)(0x10104)
 
 void IRQ_handler(uint32_t irq_mask) __attribute__((section(".IRQ_handler")));
 
 void main(void)
 {
-	//*OUTPUT = 0;
+	int32_t output = 0;
+
 	*PARAMS_ZPOS = 0;
 
 	for (;;)
 	{
-		//*OUTPUT = (*OUTPUT + 128)%(1 << 16);
+		output = (output + 128)%(1 << 16);
 
 		// Wait for the DAC to become available
 		//while (*DAC0_WAIT_FINISHED_OR_READY == 0);
 
 		// Write voltage to the DAC
-		//*DAC0_TO_SLAVE = DAC_WRITE_MASK | *OUTPUT;
+		*DAC0_TO_SLAVE = DAC_WRITE_MASK | output;
 		*DAC0_ARM = 1;
 		*DAC0_ARM = 0;
 
@@ -34,14 +33,14 @@ void main(void)
 		//	*PARAMS_ZPOS += 1;
 		//}
 
-		for (*I=0;*I<DELAY;(*I)++);
+		for (uint32_t i=0;i<DELAY;i++);
 
-		//*PARAMS_ZPOS = *ADC0_FROM_SLAVE;
+		*PARAMS_ZPOS = output;
 	}
 }
 
 void IRQ_handler(uint32_t irq_mask)
 {
-	*DAC0_ARM = 1;
-	*DAC0_ARM = 0;
+	//*DAC0_ARM = 1;
+	//*DAC0_ARM = 0;
 }
